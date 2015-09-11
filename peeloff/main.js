50a8829c9d9f390100c1c3496f15369fb192a64d
kanako ubukata <kanako@asial.co.jp>
2015-09-12 08:25:12 +0900

    initial commit

---
+++ peeloff/main.js
@@ -0,0 +1,10 @@
+﻿$(function(){
+    $(window).on("scroll", function() {
+        // スクロール量を取得
+        var scroll = $(window).scrollTop();
+        // 画像の高さを取得
+        var height = $("#over img").height();
+        // 前面のdiv要素の高さを削る
+        $("#over").css("height", height-scroll);
+    });
+});
